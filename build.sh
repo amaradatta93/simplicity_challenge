bc269fc22eaf47335db8c476e5848a6247eeaa84
Amara Datta <amaradatta@gmail.com>
2018-02-25 00:14:44 -0800

    Add angular cli to deps

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 python manage.py makemigrations
 python manage.py migrate
-npm --prefix client run-script build -- --outputPath="../static/" --base-href="/static/"
 mkdir templates
 cp ./static/index.html ./templates/index.html
